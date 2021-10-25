0e3b235cc5cde5b43322093e1d03806d5b70bb2b
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-10-25 00:00:33 -0300

    enable pgcrypto

--- db/migrate/20211025024417_create_invoices.rb
+++ db/migrate/20211025024417_create_invoices.rb
@@ -1,5 +1,7 @@
 class CreateInvoices < ActiveRecord::Migration[6.1]
   def change
+    enable_extension 'pgcrypto'
+
     create_table :invoices, id: :uuid do |t|
       t.string :number, null: false
       t.text :company, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_25_024417) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "invoices", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "number", null: false
+    t.text "company", null: false
+    t.text "billing", null: false
+    t.money "total_amount", scale: 2, null: false
+    t.string "email_list", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
