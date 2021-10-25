2dcb72446518101cd1be1f6cf132a41a0c8d778d
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-10-24 23:36:52 -0300

    create invoices table

---
+++ db/migrate/20211025022927_create_invoices_table.rb
@@ -0,0 +1,12 @@
+class CreateInvoicesTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :invoice_tables do |t|
+      t.string :number, null: false
+      t.text :company, null: false
+      t.text :billing, null: false
+      t.money :total_amount, null: false
+      t.string :email_list, null: false
+      t.timestamps
+    end
+  end
+end
