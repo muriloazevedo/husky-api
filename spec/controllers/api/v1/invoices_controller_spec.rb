5225d71493a41376205a658a50687165b997d9a7
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-10-25 14:14:30 -0300

    add support helpers; add on more spec on invoices controller

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe Api::V1::InvoicesController, type: :request do
 
     describe '#create' do
       before do
-        get "/v1/invoices/#{invoice.id}"
+        get "/v1/invoices/#{invoice.id}", as: :json
       end
 
       context 'on success' do  
         it 'returns 201' do
           expect(response).to have_http_status(:ok)
+        end
+
+        it 'returns correct type' do
+          expect(json_body[:data][:type]).to eq('invoice')
+        end
+
+        it 'returns correct attributes' do
+          expect(json_body[:data][:attributes].keys).to contain_exactly(
+            *InvoiceSerializer.attributes_to_serialize.keys
+          )
         end
       end
     end
---
+++ spec/support/helpers/json_body.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module JsonHelper
+  def json_body
+    parsed_body = JSON.parse(response.body)
+    parsed_body.deep_symbolize_keys! if parsed_body.is_a? Hash
+
+    parsed_body
+  end
+end
+
+RSpec.configure do |config|
+  config.include JsonHelper, type: :request
+end
