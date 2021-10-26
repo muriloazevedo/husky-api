51e1f6b0d4c0c154c48a0f527aa25433908430f4
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-10-26 17:28:41 -0300

    change route to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope module: 'api', format: 'json' do
     namespace :v1 do
       post 'authentication/create'
-      get 'sessions/create'
+      post 'sessions/create'
       resources :invoices, only: %i[create show index]
     end
   end
